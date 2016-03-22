3c10c7f958d51087f7c7ad1ac5a32139f89d9994
Tommyixi <carpenito.t@gmail.com>
2016-03-22 16:40:21 -0700

    Adds index page for sysadmin projects

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160322201518) do
+ActiveRecord::Schema.define(version: 20160322222122) do
 
   create_table "projects", force: true do |t|
     t.string   "title"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20160322201518) do
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "title"
   end
 
   create_table "users", force: true do |t|
