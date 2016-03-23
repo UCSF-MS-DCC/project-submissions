92b79d0297464244c437ae2a217fd2b8149e4bac
Tommyixi <carpenito.t@gmail.com>
2016-03-23 09:49:34 -0700

    Adds papertrail

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160322222122) do
+ActiveRecord::Schema.define(version: 20160323163945) do
 
   create_table "projects", force: true do |t|
     t.string   "title"
@@ -68,5 +68,16 @@ ActiveRecord::Schema.define(version: 20160322222122) do
   end
 
   add_index "users_roles", ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id"
+
+  create_table "versions", force: true do |t|
+    t.string   "item_type",                     null: false
+    t.integer  "item_id",                       null: false
+    t.string   "event",                         null: false
+    t.string   "whodunnit"
+    t.text     "object",     limit: 1073741823
+    t.datetime "created_at"
+  end
+
+  add_index "versions", ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
 
 end
