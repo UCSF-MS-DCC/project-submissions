89aeadc80692d8268ab46600a19d13d268895677
Tommyixi <carpenito.t@gmail.com>
2016-06-23 17:02:59 -0700

    Sends db to adam

--- app/mailers/database_mailer.rb
+++ app/mailers/database_mailer.rb
@@ -3,6 +3,6 @@ class DatabaseMailer < ActionMailer::Base
 
   def backup_database
   	attachments['MSDR.sqlite3'] = File.read("#{Rails.root}/db/production.sqlite3")
-  	mail(to: "thomas.carpenito@ucsf.edu", subject: 'Weekly MSDR Database')
+  	mail(to: "adam.santaniello@ucsf.edu", subject: 'Weekly MSDR Database')
   end
 end
