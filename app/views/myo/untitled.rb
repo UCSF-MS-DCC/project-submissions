4538026f0b4875dc685a7e24b78bd48da64e9d6a
Tommyixi <carpenito.t@gmail.com>
2016-06-28 09:55:36 -0700

    Fixes styling to ensure edges are aligned

--- app/views/myo/untitled.rb
+++
@@ -1,8 +0,0 @@
-
-$('#newParticipant').foundation('reveal', 'close');
-
-$('#participants').append('<%= j render("myo_participant")%></tr>');
-
-<%= add_gritter( "A new participant has been added.", :title => "Success", :time => 5000) %> 
-
-$('form').find("input[type=text], textarea").val("");
