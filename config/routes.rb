7c1becd11ddead994818c6b2de081049ba379498
Adam Renschen <adamrenschen@gmail.com>
2023-01-05 13:49:34 -0800

    ...

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-
   resources :projects, only: [:index, :new, :create]
   resources :publications, only: [:index]
   resources :fundings, only: [:index]
